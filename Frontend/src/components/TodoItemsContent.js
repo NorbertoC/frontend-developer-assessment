7de3bd429381e645567c598b2b9c300fd9c1ce38
Norberto Carosella <norberto.carosella@gmail.com>
2024-04-18 14:59:24 +1200

    Refactor App file into separate components

---
+++ Frontend/src/components/TodoItemsContent.js
@@ -0,0 +1,48 @@
+import { Button, Table } from 'react-bootstrap';
+import React, { useCallback, useState } from 'react';
+
+const TodoItemsContent = () => {
+  const [items, setItems] = useState([]);
+  
+  const getItems = useCallback(() => {})
+  
+  const handleMarkAsComplete = (item) => {
+  
+  }
+  
+  return (
+    <>
+      <h1>
+        Showing {items.length} Item(s){' '}
+        <Button variant="primary" className="pull-right" onClick={() => getItems()}>
+          Refresh
+        </Button>
+      </h1>
+      
+      <Table striped bordered hover>
+        <thead>
+        <tr>
+          <th>Id</th>
+          <th>Description</th>
+          <th>Action</th>
+        </tr>
+        </thead>
+        <tbody>
+        {items.map((item) => (
+          <tr key={item.id}>
+            <td>{item.id}</td>
+            <td>{item.description}</td>
+            <td>
+              <Button variant="warning" size="sm" onClick={() => handleMarkAsComplete(item)}>
+                Mark as completed
+              </Button>
+            </td>
+          </tr>
+        ))}
+        </tbody>
+      </Table>
+    </>
+  );
+};
+
+export default TodoItemsContent;
